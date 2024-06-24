8939baa1de29941bae5e59cf2d68f7efd413ffcf
Hugo van Schalkwyk <hugovans2000@gmail.com>
2024-06-24 13:56:16 +0200

    feat: ✨ Added the api services needed to perform CRUD on the trips.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,17 +1,36 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Firestore, collection, getDocs } from '@angular/fire/firestore';
+import {
+  Firestore,
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  updateDoc,
+} from '@angular/fire/firestore';
+import { Trip } from '../models/trips';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  constructor(
-    private http: HttpClient,
-    private firestore: Firestore
-  ) {}
+  constructor(private firestore: Firestore) {}
 
   getTrips() {
     return getDocs(collection(this.firestore, 'trips'));
+  }
+
+  addTrip(trip: Trip) {
+    return addDoc(collection(this.firestore, 'trips'), trip);
+  }
+
+  deleteTrip(tripId: string) {
+    return deleteDoc(doc(this.firestore, 'trips', tripId));
+  }
+
+  updateTrip(tripId: string, trip: Trip) {
+    return updateDoc(doc(this.firestore, 'trips', tripId), {
+      ...trip,
+    });
   }
 }
