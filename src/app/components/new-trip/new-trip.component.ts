6034af90cb717ece0a668fe0875029c7f44af78c
Hugo van Schalkwyk <hugovans2000@gmail.com>
2024-06-25 09:56:46 +0200

    feat: ✨ Added the new trip component and route.

---
+++ src/app/components/new-trip/new-trip.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-trip',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-trip.component.html',
+  styleUrl: './new-trip.component.scss'
+})
+export class NewTripComponent {
+
+}
