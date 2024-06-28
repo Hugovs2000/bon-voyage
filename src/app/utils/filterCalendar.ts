47281919f6e8ac6586371187f1985f4225d3b1f5
Hugo van Schalkwyk <hugovans2000@gmail.com>
2024-06-28 11:31:01 +0200

    fix: 🐛 Made use of nullish coalescing.

--- src/app/utils/filterCalendar.ts
+++ src/app/utils/filterCalendar.ts
@@ -1,5 +1,5 @@
 export const filterConfig = (d: Date | null): boolean => {
-  const day = d || new Date();
+  const day = d ?? new Date();
   const today = new Date(new Date().setDate(new Date().getDate() - 1));
   return day > today;
 };
