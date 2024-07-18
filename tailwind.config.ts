c1bffb94d3ab9142be451023a36dad718e1b8a55
Hugo van Schalkwyk <hugovans2000@gmail.com>
2024-07-18 09:26:32 +0200

    style: 💄 Added a bounce animation to show that the trip cards can be swiped

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,17 @@ module.exports = {
       scale: {
         '101': '1.01',
       },
+      keyframes: {
+        'bounce-x': {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '25%': { transform: 'translateX(5px)' },
+          '50%': { transform: 'translateX(0)' },
+          '75%': { transform: 'translateX(5px)' },
+        },
+      },
+      animation: {
+        'bounce-x': 'bounce-x 1s ease-in-out',
+      },
     },
   },
   plugins: [require('daisyui')],
